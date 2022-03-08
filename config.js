7297d7ef9301e5ae817a57e0f3ea59b58bcc6a31
josdinh03198 <72588136+josdinh03198@users.noreply.github.com>
2022-03-08 18:20:51 +0700

    Add files via upload
    create-card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Thoa';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100022106676874';
+const eventName = 'Chúc Mừng Ngày Quốc Tế Phụ Nữ';
+const titleCard = 'Tặng Thoa';
+const contentCard = 'Chúc Cậu một ngày mùng 8/3 thật ý nghĩa, vui tươi ngập tràn hạnh phúc. Chúc Cậu luôn cười tươi, cười duyên, cười e thẹn, cười trẻ trung, ngập tràn các kiểu cười trong ngày hôm nay.';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
